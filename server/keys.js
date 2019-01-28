e0a0c40e5267d0de64fc114c72363da42e50c8fa
Sergey Dreger <sergey.dreger@railsreactor.com>
2019-01-28 18:28:56 +0200

    initial commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    redisHost: process.env.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT,
+    pgUser: process.env.PG_USER,
+    pgPass: process.env.PG_PASS,
+    pgHost: process.env.PG_HOST,
+    pgPort: process.env.PG_PORT,
+    pgDb: process.env.PG_DB
+};
